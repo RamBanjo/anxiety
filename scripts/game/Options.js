55239de9970c00209a6fa33bbf9801f1faaa7fc4
RamBanjo <rambanjo@gmail.com>
2022-01-16 02:34:21 +0700

    few typos in index, updated credits

--- scripts/game/Options.js
+++ scripts/game/Options.js
@@ -26,7 +26,7 @@ Loader.addSounds([
 			HOW_MANY_PROMPTS = 1;
 		}
 		Game.CLICK_TO_ADVANCE = !Game.CLICK_TO_ADVANCE;
-		text_automatic_toggle.innerHTML = Game.CLICK_TO_ADVANCE ? "ด้วยคลิ๊ก" : "อัตโนมัติ";
+		text_automatic_toggle.innerHTML = Game.CLICK_TO_ADVANCE ? "ด้วยคลิก" : "อัตโนมัติ";
 
 		// Sound
 		sfx( Game.CLICK_TO_ADVANCE ? "ui_button2" : "ui_button1");
